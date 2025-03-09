1e160213b4d0f5d7ae4d3218a1f9142b6658c7e1
Abhishek Rajput <abhishek25022004@gmail.com>
2025-03-09 18:31:09 +0530

    Deployment on vercel

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,6 +4,7 @@ const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URL);
     console.log(`MongoDB Connected:`);
+    process.exit(1);
   } catch (error) {
     console.error(`Error: ${error.message}`);
     process.exit(1);
