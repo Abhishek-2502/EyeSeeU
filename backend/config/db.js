0521fc54b6788039e66a22b6fdcb827833e294b3
Abhishek Rajput <abhishek25022004@gmail.com>
2025-03-09 18:39:35 +0530

    Deployment on vercel

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,17 +2,17 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
+    if (mongoose.connection.readyState >= 1) {
+      console.log("MongoDB already connected");
+      return;
+    }
+
     await mongoose.connect(process.env.MONGO_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
 
     console.log("MongoDB Connected");
-
-    // Close the connection immediately after connecting
-    await mongoose.connection.close();
-    console.log("MongoDB Connection Closed (Vercel Optimization)");
-
   } catch (error) {
     console.error(`Error: ${error.message}`);
     process.exit(1); // Exit process on failure
