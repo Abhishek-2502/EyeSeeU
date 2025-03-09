4398a21d1b99c6a40618ffaa9c2b5d8df3da8fb5
Abhishek Rajput <abhishek25022004@gmail.com>
2025-03-09 18:46:17 +0530

    Deployment on render

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,20 +2,12 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    if (mongoose.connection.readyState >= 1) {
-      console.log("MongoDB already connected");
-      return;
-    }
+    const conn = await mongoose.connect(process.env.MONGO_URL);
+    console.log(`MongoDB Connected:`);
 
-    await mongoose.connect(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-
-    console.log("MongoDB Connected");
   } catch (error) {
     console.error(`Error: ${error.message}`);
-    process.exit(1); // Exit process on failure
+    process.exit(1);
   }
 };
 
