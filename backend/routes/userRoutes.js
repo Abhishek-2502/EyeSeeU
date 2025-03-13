88587f1bf1e77e7ef519072d81c258d0f80c54b5
Arnavjain2503 <166912655+Arnavjain2503@users.noreply.github.com>
2025-03-13 13:47:19 +0530

    frontend changes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -14,6 +14,7 @@ userRoutes.post("/auth", authUser);
 userRoutes.post("/logout", logoutUser);
 userRoutes.post("/register", registerUser);
 // protecting profile route using auth middleware protect
+
 userRoutes
   .route("/profile")
   .get(protect, getUserProfile)
