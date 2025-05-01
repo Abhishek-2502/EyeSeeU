c328bdca3141baeb8782592eee9b9f7bc6e3e03d
Abhishek-2502 <abhishek25022004@gmail.com>
2025-05-02 01:36:16 +0530

    Updated ec2 ip

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,7 +1,7 @@
 import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: 'http://localhost:5000/api',
+  baseUrl: 'http://50.19.10.187:5000/api',
   credentials: 'include', // if you're using cookies with authentication
 });
 
