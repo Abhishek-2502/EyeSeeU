494c9ead21b85424a0844a54f4e5d0bce433a169
Abhishek-2502 <abhishek25022004@gmail.com>
2025-05-02 03:02:43 +0530

    Testing with render

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,8 +1,7 @@
 import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
 
 const baseQuery = fetchBaseQuery({
-  // baseUrl: 'http://50.19.10.187:5000/api',
-  baseUrl: 'https://eyeseeu.onrender.com/api',
+  baseUrl: 'http://50.19.10.187:5000/api',
   credentials: 'include', // if you're using cookies with authentication
 });
 
