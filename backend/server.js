1a2ceb17be4a2a9a0873ad180cd25abd935cec76
Abhishek-2502 <abhishek25022004@gmail.com>
2025-05-02 03:09:02 +0530

    Testing with render

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ const port = process.env.PORT || 5000;
 
 
 app.use(cors({
-  origin: "http://50.19.10.187:3000", // your frontend URL
+  origin: "https://eye-see-u.vercel.app/", // your frontend URL
   credentials: true, // allow cookies and credentials
 }));
 
