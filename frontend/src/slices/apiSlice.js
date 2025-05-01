8667b00281b31556e173969f9b2c99312d320a1d
Abhishek-2502 <abhishek25022004@gmail.com>
2025-05-02 02:49:53 +0530

    Added render link

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,7 +1,7 @@
 import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: 'http://50.19.10.187:5000/api',
+  baseUrl: 'https://eyeseeu.onrender.com/api',
   credentials: 'include', // if you're using cookies with authentication
 });
 
